f22885f9b768111e9b40eaeb313a99c1fb4e17d8
unknown <nksreenivasan@gmail.com>
2024-08-27 18:11:50 -0400

    hw4

--- lambda_function.py
+++ lambda_function.py
@@ -8,4 +8,6 @@ def lambda_handler(event,context):
     df = pd.DataFrame(data=d)
     print(df)
 
-    print('Done x1.16')
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
+    print(df)
