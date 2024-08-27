019c6eb85c94ce1fb83f79c5aefe6ce132af8d86
unknown <nksreenivasan@gmail.com>
2024-08-27 18:50:17 -0400

    hw5

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,10 @@
-import pandas as pd
 import json
 
 def lambda_handler(event,context):
     d= {'statusCode': 200,
-        'body': json.dumps('Hello world from Git Hub to Lambda 3')
+        'body': json.dumps('Hello world from Git Hub to Lambda 4')
        }
-    df = pd.DataFrame(data=d)
+    df = json.loads(d)
     print(df)
 
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
+  
