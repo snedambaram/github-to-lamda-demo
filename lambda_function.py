120d87d86aa6c3fa0ef39c44e63f5e98081cb648
unknown <nksreenivasan@gmail.com>
2024-09-20 20:20:19 -0400

    test20

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event,context):
      x = {
         "name": "Sreeni",
-        "message": "Hello World from Github",
+        "message": "Hello World from Github1",
         "city": "Atlanta"
         }
 
